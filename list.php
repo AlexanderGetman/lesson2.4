ae20ef47cdd0972f56d1918449cbaa936542bb82
AlexanderGetman <40514260+AlexanderGetman@users.noreply.github.com>
2018-08-05 16:18:19 +1000

    Update list.php

--- list.php
+++ list.php
@@ -1,5 +1,12 @@
 <?php
+require_once './functions.php';
 header('Content-Type: text/html; charset=utf-8');
+
+if (empty($_SESSION['user'])) {
+    http_response_code(403);
+    exit('Авторизуйтесь');
+}
+
 $dir = __DIR__.'/test';
 $list = array_diff(scandir($dir), array('..', '.'));
 $counter = 1;
@@ -13,16 +20,28 @@ $counter = 1;
 </head>
 <body>
 <h1>Список тестов</h1>
+<p>Привет, <?php if($_SESSION['guest'] == false)
+{
+    echo $_SESSION['user']['username'];
+} else
+    {
+        echo $_SESSION['user'];
+    };?></p>
 <?php foreach ($list as $tests): ?>
 <li><?php echo 'Тест номер '.$counter++.' / имя файла: '.$tests.'</br>';?></li>
 <?php endforeach;?>
 </body>
 </html>
 
-<?php foreach($list as $tests)
+<?php if($_SESSION['guest'] == false)
 {
-$fileLink = $dir.'/'.$tests;
 
-echo '<a href="'.$fileLink.'">'.$tests.'</a>'.' ';
-echo '<a href="delete.php?file='.$tests.'">Удалить</a>'.'</br>';
+    foreach($list as $tests)
+    {
+    $fileLink = $dir.'/'.$tests;
+
+    echo '<a href="'.$fileLink.'">'.$tests.'</a>'.' ';
+    echo '<a href="delete.php?file='.$tests.'">Удалить</a>'.'</br>';
+    }
 }
+echo '<p><a href="logout.php">Выход</a></body></p>';
